52d938070741448efbfe2b64346aed16080a8acc
mq804-utd <mq804.utd@gmail.com>
2021-06-11 17:15:19 +0300

    dev-7 tablet without PixelPerfect

--- js/navUtilHide.js
+++ js/navUtilHide.js
@@ -1,18 +1,21 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 40) {
-        $('#navigation-utility').css({
-            'top': '-40px',
-        });
-        $('#navigation-main').css({
-            'top': '0px',
-        });
-    }
-    if ($(this).scrollTop() < 40) {
-        $('#navigation-utility').css({
-            'top': '0'
-        });
-        $('#navigation-main').css({
-            'top': '40px',
-        });
+    const menuToggle = document.getElementById('menuToggle__checkbox')
+    if (!menuToggle.checked) {
+        if ($(this).scrollTop() > 40) {
+            $('#navigation-utility').css({
+                'top': '-40px',
+            });
+            $('#navigation-main').css({
+                'top': '0px',
+            });
+        }
+        if ($(this).scrollTop() < 40) {
+            $('#navigation-utility').css({
+                'top': '0'
+            });
+            $('#navigation-main').css({
+                'top': '40px',
+            });
+        }
     }
 });
