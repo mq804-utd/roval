ae87f87c5f415210645133291dd098e14b3be4d6
mq804-utd <mq804.utd@gmail.com>
2021-06-14 14:37:51 +0300

    dev-8 PP

--- js/navUtilHide.js
+++ js/navUtilHide.js
@@ -1,21 +1,35 @@
+const backdrop = '#backdrop'
+const currentWidth = window.innerWidth
+const mobileMaxWidth = 425
+const tabletMaxWidth = 1024
+const desktopDistance = 40 //desktop move menu
+const mobileDistance = 32 //mobile move menu
+const tabletDistance = 32 //tablet move menu
+const moveDistance = (currentWidth <= tabletMaxWidth)
+    ? (currentWidth <= mobileMaxWidth)
+        ? mobileDistance
+        : tabletDistance
+    : desktopDistance
+
 $(window).scroll(function() {
-    const menuToggle = document.getElementById('menuToggle__checkbox')
-    if (!menuToggle.checked) {
-        if ($(this).scrollTop() > 40) {
-            $('#navigation-utility').css({
-                'top': '-40px',
-            });
-            $('#navigation-main').css({
-                'top': '0px',
-            });
-        }
-        if ($(this).scrollTop() < 40) {
-            $('#navigation-utility').css({
-                'top': '0'
-            });
-            $('#navigation-main').css({
-                'top': '40px',
-            });
-        }
+    if ($(this).scrollTop() > moveDistance) {
+        $('#navigation-utility').css({
+            'top': '-' + moveDistance +'px',
+        });
+        $('#navigation-main').css({
+            'top': '0px',
+        });
+        if (mobileDistance === tabletDistance)
+            $(backdrop)[0].style.top = '56px'
+    }
+    if ($(this).scrollTop() < 40) {
+        $('#navigation-utility').css({
+           'top': '0'
+        });
+        $('#navigation-main').css({
+            'top': moveDistance + 'px',
+        });
+        if (mobileDistance === tabletDistance)
+            $(backdrop)[0].style.top = '88px'
     }
 });
